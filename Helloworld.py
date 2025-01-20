5462b79c50d706bfd6ba274a5a34e2120d25577a
VJHARAN <haranvj1997@gmail.com>
2025-01-20 13:38:58 +0530

    solved exercise1

--- Helloworld.py
+++ Helloworld.py
@@ -1,3 +1,7 @@
+'''Write a program that, when run, greets the user by printing ―Hello, world!‖ on the screen. Then it
+prints a message on the screen asking the user to enter their name. The program greets the user by
+name by printing the ―Hello,‖ followed by the user’s name.'''
+
 print("Hello, world!")
 name=input("What is your name? ")
 print(f"Hello, {name}")
